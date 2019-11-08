35f85175cdd110158c5aee20a0a4cb677850f7a3
Jasyn Marais <jasynmarais@gmail.com>
2019-11-08 12:12:49 +0200

    Add styling to reach MVP.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import { CharacterList } from './components/CharacterList';
 
 const App = () => {
   const [characters, setCharacters] = useState([]);
@@ -17,6 +18,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CharacterList data={characters} />
     </div>
   );
 };
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CharacterCard = styled.div`
+    width: 500px;
+    padding: 1.1rem;
+    margin: 1rem 0;
+    background: #f0db4f;
+    border-radius: 11px;
+    box-shadow: 3px 3px 4px #323330;
+    transition: ease-in-out 0.2s;
+
+    &:hover {
+        transform: scale(1.03);
+        transition: ease-in-out 0.13s;
+        box-shadow: 2px 5px 7px #323330;
+    }
+
+    @media (max-width: 500px) {
+        width: 85%;
+    }
+
+    h2 {
+        color: #323330;
+        font-size: 2rem;
+    }
+
+    p {
+        font-size: 1.4rem;
+        color: #323330;
+    }
+`;
 
 export const Character = ({ data }) => {
     return (
-        <div>
+        <CharacterCard>
             <h2>{data.name}</h2>
             <p>
             <span>Hair Color: </span>
@@ -20,6 +51,6 @@ export const Character = ({ data }) => {
              <span>DOB: </span>
                 {data.birth_year}
             </p>
-        </div>
+        </CharacterCard>
     );
 };
